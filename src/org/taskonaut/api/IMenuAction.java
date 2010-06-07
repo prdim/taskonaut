9474d0ad140b866c8f7751b217bd2d65fa4b9445
spec@jimbot.ru <spec@jimbot.ru>
2010-06-07 23:47:57 +0600

    ...

---
+++ src/org/taskonaut/api/IMenuAction.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.taskonaut.api;
+
+import javax.swing.Action;
+
+
+/**
+ * @author spec
+ *
+ */
+public interface IMenuAction {
+	public Action getAction();
+}
---
+++ taskonaut_test_actions/src/org/taskonaut/test/actions/TestActions.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.taskonaut.test.actions;
+
+import org.jdesktop.application.Action;
+
+/**
+ * @author spec
+ *
+ */
+public class TestActions {
+	
+	@Action
+	public void testAction1() {
+		System.out.println("Test action!");
+	}
+}
