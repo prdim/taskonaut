8d069ea0f526f37a4122473eae44f7d3a52076eb
spec@jimbot.ru <spec@jimbot.ru>
2010-07-13 16:36:13 +0600

    ...

--- taskonaut_tasks_gui/src/org/taskonaut/tasks/gui/NewTaskAction.java
+++ taskonaut_tasks_gui/src/org/taskonaut/tasks/gui/NewTaskAction.java
@@ -19,18 +19,37 @@ import org.taskonaut.tasks.OneTask;
 public class NewTaskAction implements IMenuAction {
 	
 	@Action
-	public Task newTaskAction() {
-//		EditTaskDialog d = new EditTaskDialog(new JFrame(), true);
-		DefaultDialog d = new DefaultDialog(new JFrame(), true);
-//		d.setTaskPanel(new EditTaskPanel(new OneTask()));
-		EditTaskPanel p = new EditTaskPanel(new OneTask());
-		d.setPanel(p);
-		d.setTitle("Создание новой задачи");
-		d.setVisible(true);
-		if(p.ok) {
-			return new MainApplication.MessageTask(MainApplication.getInstance(), "Новая задача создана");
-		}
-		return null;
+	public Task<Void,Void> newTaskAction() {
+		Task<Void,Void> t = new Task<Void,Void>(MainApplication.getInstance()) {
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				DefaultDialog d = new DefaultDialog(new JFrame(), true);
+				EditTaskPanel p = new EditTaskPanel(new OneTask());
+				d.setPanel(p);
+				d.setTitle("Создание новой задачи");
+				d.setVisible(true);
+				if(p.ok) {
+					setMessage("Новая задача создана");
+				} else {
+					setMessage("Операция отменена пользователем");
+				}
+				return null;
+			}
+			
+		};
+		return t;
+////		EditTaskDialog d = new EditTaskDialog(new JFrame(), true);
+//		DefaultDialog d = new DefaultDialog(new JFrame(), true);
+////		d.setTaskPanel(new EditTaskPanel(new OneTask()));
+//		EditTaskPanel p = new EditTaskPanel(new OneTask());
+//		d.setPanel(p);
+//		d.setTitle("Создание новой задачи");
+//		d.setVisible(true);
+//		if(p.ok) {
+//			return new MainApplication.MessageTask(MainApplication.getInstance(), "Новая задача создана");
+//		}
+//		return null;
 	}
 
 	/* (non-Javadoc)
