9b5986a8022767db4dc9df8b39f1a6dd3cd68eb5
spec@jimbot.ru <spec@jimbot.ru>
2010-08-05 22:00:53 +0600

    Навожу красоту в табличке

--- taskonaut_tasks_gui/src/org/taskonaut/tasks/gui/TaskListPanel.java
+++ taskonaut_tasks_gui/src/org/taskonaut/tasks/gui/TaskListPanel.java
@@ -11,13 +11,18 @@ import java.util.Vector;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
 
 import com.jgoodies.forms.factories.*;
 import com.jgoodies.forms.layout.*;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.Task;
 import org.jdesktop.swingx.*;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
+import org.taskonaut.app.MainApplication;
 import org.taskonaut.tasks.OneTask;
 import org.taskonaut.tasks.TaskList;
 import org.taskonaut.tasks.gui.internal.Activator;
@@ -32,14 +37,26 @@ public class TaskListPanel extends JPanelExt {
 	private ChangeTaskEventHandler eventHandler;
 	
 	/**
-	 * 
+	 * Конструктор
 	 */
 	public TaskListPanel() {
 //		super();
 		initComponents();
-		xTable1.setModel(new TaskListTableModel());
+		attachTableModel();
 		eventHandler = new ChangeTaskEventHandler();
 		Activator.regEventHandler(eventHandler, getHandlerServiceProperties("org/taskonaut/tasks/gui/events/*"));
+	}
+	
+	private void attachTableModel() {
+		xTable1.setModel(new TaskListTableModel());
+		xTable1.getColumnModel().getColumn(0).setCellRenderer(new StatusCellRenderer());
+		xTable1.getColumnModel().getColumn(1).setCellRenderer(new PriorityCellRenderer());
+		xTable1.setColumnControlVisible(true);
+		xTable1.setRowHeight(22); // Подгоняем высоту строки под иконку
+		xTable1.setAutoResizeMode(JXTable.AUTO_RESIZE_OFF);
+		xTable1.packAll();
+		xTable1.getColumnModel().getColumn(0).setPreferredWidth(24);
+		xTable1.getColumnModel().getColumn(1).setPreferredWidth(24);
 	}
 
 	/* (non-Javadoc)
@@ -60,38 +77,202 @@ public class TaskListPanel extends JPanelExt {
 		Activator.unregEventHandler(eventHandler);
 	}
 
+	/**
+	 * По двойному клику на строке вызываем процедуру редактирования задачи
+	 * @param e
+	 */
 	private void xTable1MouseClicked(MouseEvent e) {
-		// TODO add your code here
+		if(e.getClickCount()>=2) {
+//			System.out.println(xTable1.convertRowIndexToModel(xTable1.getSelectedRow()));
+			OneTask t = ((TaskListTableModel)xTable1.getModel()).
+				getData().get(xTable1.convertRowIndexToModel(xTable1.getSelectedRow()));
+			MainApplication.getInstance().getContext().getTaskService().
+				execute(new TaskEdit(MainApplication.getInstance(), t));
+		}
 	}
 
+	/**
+	 * Закрываем панель при нажатии на кнопку
+	 * @param e
+	 */
 	private void okButtonActionPerformed(ActionEvent e) {
 		parentPanel.onClose();
 	}
 
+	/**
+	 * Обработка реакции на кнопку фильтра
+	 * @param e
+	 */
 	private void filterButtonActionPerformed(ActionEvent e) {
 		// TODO add your code here
 	}
 	
+	/**
+	 * Task редактирования задачи
+	 * @author spec
+	 *
+	 */
+	private class TaskEdit extends Task<Void, Void> {
+		private OneTask ts;
+
+		public TaskEdit(Application application) {
+			super(application);
+		}
+		
+		public TaskEdit(Application app, OneTask t) {
+			super(app);
+			ts = t;
+		}
+
+		@Override
+		protected Void doInBackground() throws Exception {
+			DefaultDialog d = new DefaultDialog(new JFrame(), true);
+			EditTaskPanel p = new EditTaskPanel(ts);
+			d.setPanel(p);
+			d.setTitle("Редактирование задачи");
+			d.setVisible(true);
+			if(p.ok) {
+				setMessage("Задача изменена");
+				TaskList.getInstance().putTask(p.getTask());
+				Activator.getEventAdmin().postEvent(
+						new Event("org/taskonaut/tasks/gui/events/edit_task", 
+								getProperties(p.getTask().getId())));
+				// TODO запись в БД
+			} else {
+				setMessage("Операция отменена пользователем");
+			}
+			return null;
+		}
+		
+		private Dictionary<String, Object> getProperties(Long id) {
+			Dictionary<String, Object> result = new Hashtable<String, Object>();
+			result.put("task_id", id);
+			return result;
+		}
+		
+	}
+	
+	/**
+	 * Обработчик события изменения задачи
+	 * Необходимо обновить список задач
+	 * @author spec
+	 *
+	 */
 	private class ChangeTaskEventHandler implements EventHandler {
 
 		@Override
 		public void handleEvent(Event event) {
 			System.out.println("Event: " + event.getTopic());
 			System.out.println("Change task: " + event.getProperty("task_id"));
-			xTable1.setModel(new TaskListTableModel());
-			contentPanel.repaint();
+			if("org/taskonaut/tasks/gui/events/edit_task".equals(event.getTopic())) {
+				// При редактировании задачи просто обновим данные в таблице
+				((TaskListTableModel)xTable1.getModel()).updateData();
+//				xTable1.packAll();
+				contentPanel.repaint();
+			} else {
+				// При добавлении новых данных нужно переформировать таблицу полностью
+				attachTableModel();
+				contentPanel.repaint();
+			}
 		}
 		
 	}
 	
-	protected Dictionary<String, Object> getHandlerServiceProperties(String... topics) {
+	/**
+	 * Возвращает фильтр по топику события
+	 * @param topics
+	 * @return
+	 */
+	private Dictionary<String, Object> getHandlerServiceProperties(String... topics) {
 	    Dictionary<String, Object> result = new Hashtable<String, Object>();
 	    result.put(EventConstants.EVENT_TOPIC, topics);
 	    return result;
 	  }
 	
+	/**
+	 * Рисует ячейку таблицы со статусом задачи
+	 * @author spec
+	 *
+	 */
+	private class StatusCellRenderer implements TableCellRenderer {
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table,
+				Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+			JPanel p = new JPanel(new BorderLayout());
+			if(OneTask.Status.запланирована.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_chronometer");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Status.запланирована.name());
+			} else if(OneTask.Status.выполняется.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_player_time");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Status.выполняется.name());
+			} else if(OneTask.Status.выполнена.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_success");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Status.выполнена.name());
+			} else if(OneTask.Status.отложена.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_cancel2");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Status.отложена.name());
+			} else if(OneTask.Status.отменена.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_cancel1");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Status.отменена.name());
+			}
+			return p;
+		}
+		
+	}
+	
+	/**
+	 * Рисует ячейку таблицы с приоритетом задачи
+	 * @author spec
+	 *
+	 */
+	private class PriorityCellRenderer implements TableCellRenderer {
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table,
+				Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+			JPanel p = new JPanel(new BorderLayout());
+			if(OneTask.Priority.средний.equals(value)) {
+				// Ячейка остается пустой
+				p.setToolTipText(OneTask.Priority.средний.name());
+			} else if(OneTask.Priority.низкий.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_down");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Priority.низкий.name());
+			} else if(OneTask.Priority.высокий.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_up");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Priority.высокий.name());
+			} else if(OneTask.Priority.критично.equals(value)) {
+				Icon ico = MainApplication.getInstance().getContext().
+					getResourceMap(TaskListPanel.class).getIcon("ico_critical");
+				p.add(new JLabel(ico));
+				p.setToolTipText(OneTask.Priority.критично.name());
+			}
+			return p;
+		}
+		
+	}
+	
+	/**
+	 * Класс модели таблицы со списком задач
+	 * @author spec
+	 *
+	 */
 	private class TaskListTableModel extends AbstractTableModel {
-		private String[] columns = {"Задача"};
+		private String[] columns = {"Статус", "Приоритет", "Задача", "Срок"};
 		private Vector<OneTask> data;
 
 		/**
@@ -102,6 +283,19 @@ public class TaskListPanel extends JPanelExt {
 			for(long i : TaskList.getInstance().getTaskList().keySet()) {
 				data.add(TaskList.getInstance().getTask(i));
 			}
+		}
+		
+		/**
+		 * Обновить данные в таблицы после их изменения
+		 */
+		public void updateData() {
+			for(int i=0; i<data.size(); i++) {
+				data.set(i, TaskList.getInstance().getTask(data.get(i).getId()));
+			}
+		}
+		
+		public Vector<OneTask> getData() {
+			return data;
 		}
 
 		/* (non-Javadoc)
@@ -124,11 +318,24 @@ public class TaskListPanel extends JPanelExt {
 
 		@Override
 		public Object getValueAt(int r, int c) {
+			switch(c) {
+			case 0:
+				return data.get(r).getState();
+			case 1:
+				return data.get(r).getPriority();
+			case 2:
+				return data.get(r).getName();
+			case 3:
+				return data.get(r).getExecute();
+			}
 			return data.get(r).getName();
 		}
 		
 	}
 
+	/**
+	 * Инициализация всех компонентов формы
+	 */
 	private void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
 //		dialogPane = new JPanel();
