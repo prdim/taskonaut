552e7384fee615c109bc29589a93f80f09db1c49
spec@jimbot.ru <spec@jimbot.ru>
2010-11-07 23:51:41 +0500

    Тест экспорта в iCal

--- taskonaut_test_actions/src/org/taskonaut/test/actions/TestActions.java
+++ taskonaut_test_actions/src/org/taskonaut/test/actions/TestActions.java
@@ -5,11 +5,28 @@ package org.taskonaut.test.actions;
 
 import java.beans.PropertyDescriptor;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.SocketException;
+
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.ValidationException;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.parameter.Value;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Version;
+import net.fortuna.ical4j.util.UidGenerator;
 
 import org.apache.commons.beanutils.PropertyUtils;
 import org.jdesktop.application.Action;
 import org.taskonaut.api.IMenuAction;
+import org.taskonaut.api.tasks.TaskItem;
+import org.taskonaut.api.tasks.TaskStoreServiceConnector;
 import org.taskonaut.test.actions.internal.Activator;
 
 /**
@@ -29,11 +46,45 @@ public class TestActions implements IMenuAction {
 //		GuiConfig.getInstance().setHideMinimized(true);
 //		GuiConfig.getInstance().save();
 		
-		System.out.println(Activator.getProps().getTitle());
-//		PropertyUtils.setDebug(1);
-		PropertyDescriptor[] pr = PropertyUtils.getPropertyDescriptors(Activator.getProps());
-		for(PropertyDescriptor p : pr) {
-			System.out.println(p.getDisplayName() + " : " + PropertyUtils.getProperty(Activator.getProps(), p.getName()));
+		// Тест формата iCal
+		Calendar calendar = new Calendar();
+		calendar.getProperties().add(new ProdId("-//Taskonaut//iCal4j 1.0//EN"));
+		calendar.getProperties().add(Version.VERSION_2_0);
+		calendar.getProperties().add(CalScale.GREGORIAN);
+		UidGenerator ug = null;
+		try {
+			ug = new UidGenerator("1");
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(TaskItem i : TaskStoreServiceConnector.getStore().readAllTasks()) {
+			if(i.getExecute()> System.currentTimeMillis()) { // Имеет смысл выгружать только будущие события
+			VEvent ev = new VEvent(new Date(i.getExecute()), i.getName());
+			// initialise as an all-day event..
+//			ev.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
+//			try {
+//				ug = new UidGenerator(String.valueOf(i.getID()));
+//			} catch (SocketException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			ev.getProperties().add(ug.generateUid());
+			
+			calendar.getComponents().add(ev);
+			}
+		}
+//		System.out.println(calendar);
+		FileOutputStream fout = new FileOutputStream("./mycalendar.ics");
+		CalendarOutputter outputter = new CalendarOutputter();
+		try {
+			outputter.output(calendar, fout);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ValidationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
